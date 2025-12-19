f4764d5e7b2ecc012c70412ecdf09406f599bf88
Matt <github@mattcast.me>
2025-12-19 17:27:36 -0500

    Add Foundry dedicated server support with GHCR workflow

--- README.md
+++ README.md
@@ -1,18 +1,9 @@
 # SteamCMD in Docker optimized for Unraid
-This Docker will download and install SteamCMD. It will also install Astroneer and run it.
+This Docker will download and install SteamCMD. It will also install Foundry Dedicated Server and run it.
 
-**ATTENTION:** You have to add this entry to the Engine.ini from the clients which are going to connecting to this dedicated server:
-```
-[SystemSettings]
-net.AllowEncryption=False
-```
-You'll find the file on your local Windows machine at: `%localappdata%\Astro\Saved\Config\WindowsNoEditor`
+**Foundry** is a first-person factory building game where you construct and automate factories on alien planets.
 
-**WARNING:** If a client tries to connect to the Dedicated Server which don't has this entry in the Engine.ini the server will be left in a semi bricked state and you have to restart the Docker container.
-
-**Servername:** 'AstroneerDocker' Password: 'Docker' ConsolePassword: 'adminDocker'
-
-**Serveradmin:** If you want to become a server admin then stop the container and edit the file ../Astro/Saved/Config/WindowsServer/AstroServerSettings.ini and add your Steam name after `OwnerName=` eg: `OwnerName=YourSteamName`, after that start the container and connect to the Dedicated Server.
+**ATTENTION:** First startup can take very long since it downloads the game server files!
 
 **Update Notice:** Simply restart the container if a newer version of the game is available.
 
@@ -21,9 +12,10 @@ You'll find the file on your local Windows machine at: `%localappdata%\Astro\Sav
 | --- | --- | --- |
 | STEAMCMD_DIR | Folder for SteamCMD | /serverdata/steamcmd |
 | SERVER_DIR | Folder for gamefile | /serverdata/serverfiles |
+| GAME_ID | The Steam App ID for Foundry Dedicated Server | 2915550 |
 | GAME_PARAMS | Values to start the server if needed. | empty |
 | UPDATE_PUBLIC_IP | If set to 'true' the container will check on each container start if the Public IP is still valid. | false |
-| BACKUP | Set this value to 'true' to enable the automated backup function from the container, you find the Backups in '.../astroneer/Backups/'. Set to 'false' to disable the backup function. | true |
+| BACKUP | Set this value to 'true' to enable the automated backup function from the container, you find the Backups in '.../foundry/Backups/'. Set to 'false' to disable the backup function. | true |
 | BACKUP_INTERVAL | The backup interval in minutes (ATTENTION: The first backup will be triggered after the set interval in this variable after the start/restart of the container) | 360 |
 | BACKUPS_TO_KEEP | Number of backups to keep (by default set to 8 to keep the last backups of the last 48 hours) | 8 |
 | UID | User Identifier | 99 |
@@ -35,9 +27,11 @@ You'll find the file on your local Windows machine at: `%localappdata%\Astro\Sav
 
 ## Run example
 ```
-docker run --name Astroneer -d \
-	-p 8777:8777/udp \
-	--env 'GAME_ID=728470' \
+docker run --name Foundry -d \
+	-p 15777:15777/udp \
+	-p 15777:15777/tcp \
+	-p 7777:7777/udp \
+	--env 'GAME_ID=2915550' \
 	--env 'UPDATE_PUBLIC_IP=false' \
 	--env 'BACKUP=true' \
 	--env 'BACKUP_INTERVAL=360' \
@@ -45,12 +39,8 @@ docker run --name Astroneer -d \
 	--env 'UID=99' \
 	--env 'GID=100' \
 	--volume /path/to/steamcmd:/serverdata/steamcmd \
-	--volume /path/to/astroneer:/serverdata/serverfiles \
-	ich777/steamcmd:astroneer
+	--volume /path/to/foundry:/serverdata/serverfiles \
+	ghcr.io/YOUR_USERNAME/docker-steamcmd-server:foundry
 ```
 
 This Docker was mainly edited for better use with Unraid, if you don't use Unraid you should definitely try it!
-
-This Docker is forked from mattieserver, thank you for this wonderfull Docker.
-
-### Support Thread: https://forums.unraid.net/topic/79530-support-ich777-gameserver-dockers/
--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -55,72 +55,12 @@ else
 fi
 
 echo "---Prepare Server---"
-echo "---Looking for config files---"
-if [ ! -d ${SERVER_DIR}/Astro/Saved/Config/WindowsServer ]; then
-  mkdir -p ${SERVER_DIR}/Astro/Saved/Config/WindowsServer
-fi
-if [ ! -f ${SERVER_DIR}/Astro/Saved/Config/WindowsServer/AstroServerSettings.ini ]; then
-  echo "[/Script/Astro.AstroServerSettings]
-bLoadAutoSave=True
-MaxServerFramerate=30.000000
-MaxServerIdleFramerate=3.000000
-bWaitForPlayersBeforeShutdown=False
-ConsolePassword=adminDocker
-PublicIP=
-ServerName=AstroneerDocker
-ServerGuid=
-OwnerName=
-OwnerGuid=0
-PlayerActivityTimeout=0
-ServerPassword=Docker
-bDisableServerTravel=False
-DenyUnlistedPlayers=False
-VerbosePlayerProperties=False
-AutoSaveGameInterval=900
-BackupSaveGamesInterval=7200
-ActiveSaveFileDescriptiveName=SAVE_1
-ServerAdvertisedName=" > ${SERVER_DIR}/Astro/Saved/Config/WindowsServer/AstroServerSettings.ini
+echo "---Checking for config directory---"
+if [ ! -d ${SERVER_DIR}/FactoryGame/Saved/Config/WindowsServer ]; then
+  mkdir -p ${SERVER_DIR}/FactoryGame/Saved/Config/WindowsServer
+  echo "---Config directory created---"
 else
-  echo "---'AstroServerSettings.ini' found---"
-fi
-if [ ! -f ${SERVER_DIR}/Astro/Saved/Config/WindowsServer/Engine.ini ]; then
-  echo "[URL]
-Port=8777
-
-[SystemSettings]
-net.AllowEncryption=False" > ${SERVER_DIR}/Astro/Saved/Config/WindowsServer/Engine.ini
-else
-  echo "---'Engine.ini' found---"
-  sed -i '/^HeartbeatInterval=0/d' ${SERVER_DIR}/Astro/Saved/Config/WindowsServer/AstroServerSettings.ini
-fi
-
-echo "---Checking if PublicIP is in place---"
-PUBLIC_IP="$(cat ${SERVER_DIR}/Astro/Saved/Config/WindowsServer/AstroServerSettings.ini | grep "PublicIP=" | cut -d '=' -f2)"
-if [ -z "${PUBLIC_IP}" ]; then
-  echo "---No PublicIP found in AstroServerSettings.ini, trying to obtain it...---"
-  PUBLIC_IP="$(wget -qO - icanhazip.com)"
-  if [ -z "${PUBLIC_IP}" ]; then
-    echo "---Can't get PublicIP, please set it manually in your AstroServerSettings.ini!---"
-  else
-    echo "---Sucessfully obtained PublicIP: ${PUBLIC_IP}, adding to AstroServerSettings.ini"
-    sed -i "s/PublicIP=.*/PublicIP=${PUBLIC_IP}/g" ${SERVER_DIR}/Astro/Saved/Config/WindowsServer/AstroServerSettings.ini
-  fi
-else
-  if [ "${UPDATE_PUBLIC_IP}" == "true" ]; then
-    NEW_PUBLIC_IP="$(wget -qO - icanhazip.com)"
-    if [ -z "${NEW_PUBLIC_IP}" ]; then
-      echo "---Can't get PublicIP, please set it manually in your AstroServerSettings.ini!---"
-    else
-      if [ "${PUBLIC_IP}" != "${NEW_PUBLIC_IP}" ]; then
-        echo "---Changing PublicIP in AstroServerSettings.ini to: ${NEW_PUBLIC_IP}!---"
-        sed -i "s/PublicIP=.*/PublicIP=${NEW_PUBLIC_IP}/g" ${SERVER_DIR}/Astro/Saved/Config/WindowsServer/AstroServerSettings.ini
-      else
-        echo "---Nothing to do, PublicIP: ${PUBLIC_IP} still up-to-date!---"
-      fi
-    fi
-  else
-    echo "---PublicIP in AstroServerSettings.ini found: ${PUBLIC_IP}"
-  fi
+  echo "---Config directory found---"
 fi
 
 export WINEARCH=win64
@@ -148,19 +88,9 @@ chmod -R ${DATA_PERM} ${DATA_DIR}
 echo "---Server ready---"
 echo
 echo "+--------------------------------------------------------------------------"
-echo "| Please don't forget to add this entry to your Engine.ini on your Clients:"
+echo "| Foundry Dedicated Server"
+echo "| Server is ready to accept connections"
 echo "+--------------------------------------------------------------------------"
-echo
-echo "[SystemSettings]"
-echo "net.AllowEncryption=False"
-echo
-echo "+-------------------------------------------------------------------------------"
-echo "| ATTENTION: If a client tries to connect without that in Engine.ini, the server"
-echo "| will be left in a semi bricked state and you have to restart the container!"
-echo "|"
-echo "| You can find the file on your local Windows machine at:"
-echo "| %localappdata%\Astro\Saved\Config\WindowsNoEditor"
-echo "+-------------------------------------------------------------------------------"
 echo
 
 if [ "${BACKUP}" == "true" ]; then
@@ -173,4 +103,4 @@ fi
 
 echo "---Start Server---"
 cd ${SERVER_DIR}
-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' wine64 AstroServer.exe ${GAME_PARAMS}
+xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' wine64 FoundryDedicatedServer.exe ${GAME_PARAMS}
